63e2972e142b599df849ab58716d810ea7d1d450
nicktyson <fake@leavemealone.com>
2013-05-01 23:10:00 -0400

    exporter supports texture coordinates

--- blender_msh_exporter.py
+++ blender_msh_exporter.py
@@ -1,15 +1,18 @@
 import bpy
 import os
 
-# To run, Blender's working directory should be the rome folder.
+# I'd rather not use an absolute file path, but it's a little simpler.
+
+# To use relative path, Blender's working directory should be the rome folder.
 # import os
 # os.chdir()
 
 # sharp edges need to be split for them to look correct
+# the same is true for UV seams
 # quads are automatically converted to triangles
 
 current_obj = bpy.context.active_object
-f = open('Assets/Meshes/castle.msh', 'w')
+f = open('C:/Users/Nick/Documents/Visual Studio 2010/Projects/rome/Assets/Meshes/test_cube2.msh', 'w')
 
 bpy.ops.object.mode_set(mode='EDIT')
 bpy.ops.mesh.select_all() 
@@ -42,3 +45,17 @@ for vert in vert_list:
     f.write(str(round(vert.normal.x, 4))+'\n')
     f.write(str(round(vert.normal.y, 4))+'\n')
     f.write(str(round(vert.normal.z, 4))+'\n') 
+
+f.write('texcoords\n')
+
+uvdata = current_obj.data.uv_layers.active.data
+txcoords = [0] * (2*len(vert_list))
+
+for i, uv in enumerate(uvdata):
+    index = current_obj.data.loops[i].vertex_index
+    tx = uvdata[i].uv
+    txcoords[2*index] = tx.x
+    txcoords[2*index + 1] = tx.y
+    
+for tex in txcoords:
+    f.write(str(tex)+'\n')
